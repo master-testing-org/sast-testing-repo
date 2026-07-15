596a125cf0b283568db0c9f000c045ee49f40606
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-15 17:18:44 +0530

    test

--- sample.java
+++ sample.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 public class Sample {
     public static void main(String[] args) throws IOException {
         String password = System.getenv("APP_PASSWORD");
-        System.out.println("Received password: " + password);
         // Command Injection (SAST finding)[
         if (args.length > 0) {
             String command = "ping " + args[0];
