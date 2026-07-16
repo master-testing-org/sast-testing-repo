76da757aaa14787cc919a7c925de5d22ba163e2b
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-16 10:16:46 +0530

    Fix typo in comment about password usage

--- sample.java
+++ sample.java
@@ -6,7 +6,7 @@ public class Sample {
         String envPassword = System.getenv("APP_PASSWORD");
         char[] password = envPassword != null ? envPassword.toCharArray() : new char[0];
         try {
-            // use password herekk
+            // use password herekkii
         } finally {
             Arrays.fill(password, '\0');
         }
