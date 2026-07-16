0cbeb46a66dd859acbb0ad373aac183f55881c8b
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-16 10:30:55 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -6,7 +6,7 @@ public class Sample {
         String envPassword = System.getenv("APP_PASSWORD");
         char[] password = envPassword != null ? envPassword.toCharArray() : new char[0];
         try {
-            // use password herekki
+            // use password herek
         } finally {
             Arrays.fill(password, '\0');
         }
